cbe7b74a7dd5a892d3768f16ff43b49b1827cffa
Valeri Elashvili <valerielashvili@gmail.com>
2025-09-22 00:22:23 +0200

    Add scoring system & rank board

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -7,13 +7,16 @@ yellow = "\033[1;33m"
 red = "\033[0;31m"
 reset = "\033[0m"
 
+# Game variables
 rock = 'Rock'
 paper = 'Paper'
 scissors = 'Scissors'
-restart_game = ""
+restart_game = ''
+score = {'user': {'win': 0, 'loss': 0, 'rank': 0}, 'computer': {'win': 0, 'loss': 0, 'rank': 0}}
+draw_count = 0
 
-while restart_game != "no":
-    # Take user input and assign user moves
+while restart_game != 'no':
+    # Take user input & assign user moves
     player_move = input("Choose [r]ock, [p]aper or [s]cissors: ")
 
     if player_move == 'r':
@@ -25,7 +28,7 @@ while restart_game != "no":
     else:
         raise SystemExit("Invalid input. Try again...")
 
-    # Randomize and assign computer moves
+    # Randomize & assign computer moves
     computer_random_number = random.randint(1, 3)
     computer_move = ""
 
@@ -38,24 +41,68 @@ while restart_game != "no":
 
     print(f"{blue}The computer chose {computer_move}.{reset}")
 
-    # Check the winner
+    # Check the winner & assign scores
     if (player_move == rock and computer_move == scissors) or \
             (player_move == paper and computer_move == rock) or \
             (player_move == scissors and computer_move == scissors):
-        print(f"{green}You win!{reset}")
+        score['user']['win'] += 1
+        score['computer']['loss'] += 1
+        print(f"{green}You win!{reset}\n")
+
     elif (player_move == rock and computer_move == rock) or \
             (player_move == paper and computer_move == paper) or \
             (player_move == scissors and computer_move == scissors):
-        print(f"{yellow}Draw{reset}")
+        draw_count += 1
+        print(f"{yellow}Draw{reset}\n")
     else:
-        print(f"{red}You lose!{reset}")
+        score['user']['loss'] += 1
+        score['computer']['win'] += 1
+        print(f"{red}You lose!{reset}\n")
 
     restart_game = input("Type [yes] to Play Again or [no] to quit: ")
 
     if restart_game == 'yes':
         continue
     elif restart_game == 'no':
-        print("Thank you for playing!")
+
+        # Assign score ranks
+        if score['user']['win'] > score['computer']['win']:
+            score['user']['rank'] = 1
+            score['computer']['rank'] = 2
+
+        elif score['computer']['win'] > score['user']['win']:
+            score['computer']['rank'] = 1
+            score['user']['rank'] = 2
+        else:
+            score['user']['rank'] = 1
+            score['computer']['rank'] = 1
+
+        # Sort players by rank
+        score = dict(sorted(score.items(), key=lambda item: item[1]["rank"]))
+        output_lines = []
+        score_stats = ""
+
+        # Format output
+        for player, stats in score.items():
+            space_num = ""
+            if player == 'user':
+                space_num = " " * 6
+            else:
+                space_num = "  "
+            output_lines.append(f"{stats['rank']}     {player}{space_num}{stats['win']}     {stats['loss']}\n"
+                                f"----------------------------")
+        score_stats = "\n".join(output_lines)
+
+        score_board = f"""\nThank you for playing!
+
+Latest Scores:
+
+RANK  NAME      WINS  LOSSES
+============================
+{score_stats}
+Draws: {draw_count}
+"""
+        print(score_board)
         break
     else:
         raise SystemExit("Invalid input. Try again...")
